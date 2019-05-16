f7d262643f5200c782fdcd756a86b6125d835d20
Danh Nguyen <danhnguyen267@gmail.com>
2019-05-16 02:04:05 -0700

    Creating dataset and shuffling

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.*;
 import java.io.*;
+
 
 /**
  * SOURCES USED
  * https://stackoverflow.com/questions/17750575/filewriter-in-java-not-writing-to-txt-file
  * https://www.deadcoderising.com/how-to-generate-a-stream-of-random-numbers-in-java/
  * https://stackoverflow.com/questions/20058366/shuffle-a-list-of-integers-with-java-8-streams-api
+ * https://www.geeksforgeeks.org/program-to-convert-array-to-list-in-java/
  */
 
 public class Main {
@@ -15,9 +18,9 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> nums = getDataset();
-//        for (int num : nums) {
-//            System.out.println(num + "");
-//        }
+        for (int num : nums) {
+            System.out.println(num);
+        }
 
         try {
 
@@ -45,7 +48,6 @@ public class Main {
 
         }
 
-
     }
 
     /**
@@ -57,12 +59,11 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new FileReader(file));
         int lineCount = 0;
-
         String st;
         while ((st = br.readLine()) != null) {
             lineCount += 1;
 
-            if (Integer.parseInt(st) > 11) {
+            if (Integer.parseInt(st) > 19) {
                 System.out.println("Number " + st + " found at line " + lineCount);
 
             }
@@ -72,16 +73,38 @@ public class Main {
     public static List<Integer> getDataset() {
         Random gen = new Random();
 
-        IntStream firstDistribution = gen.ints(20, 1, 13);
-        IntStream secondDis = gen.ints(10, 13, 14);
+        IntStream firstDistribution = gen.ints(83000, 1, 13);
+        IntStream secondDis = gen.ints(1000, 13, 14);
+        IntStream thirdDis = gen.ints(500, 14, 15);
+        IntStream fourthDis = gen.ints(250, 15,16);
+        IntStream fifthDis = gen.ints(100, 16,17);
+        IntStream sixthDis = gen.ints(50, 17,18);
+        IntStream seventhDis = gen.ints(25, 18,19);
+        IntStream eighthDis = gen.ints(10, 19,20);
+        IntStream lastDis = gen.ints(5, 20,21);
+
+
 
         IntStream total = IntStream.concat(firstDistribution, secondDis);
+        total = IntStream.concat(total, thirdDis);
+        total = IntStream.concat(total, fourthDis);
+        total = IntStream.concat(total, fifthDis);
+        total = IntStream.concat(total, sixthDis);
+        total = IntStream.concat(total, seventhDis);
+        total = IntStream.concat(total, eighthDis);
+        total = IntStream.concat(total, lastDis);
+
 
         List<Integer> integers =
-                        total                                   // <-- take our combined IntStream dataset
-                        .boxed()                                // <-- converts them to Integers
+                        total       // <-- take our combined IntStream dataset
+                        .boxed() // <-- converts them to Integers
                         .collect(Collectors.toList());          // <-- collects the values to a list
 
+        Collections.sort(integers);
+
+        Collections.shuffle(integers, new Random(System.nanoTime()));
+
+        // shuffle twice for assurance of no consecutives
         Collections.shuffle(integers, new Random(System.nanoTime()));
 
         return integers;
