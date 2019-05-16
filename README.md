beae86f4f8ead5715dd914ef3c0618fcbde81c48
Danh Nguyen <danhnguyen267@gmail.com>
2019-05-15 23:35:22 -0700

    Creating intstreams and shuffling them

---
+++ README.md
@@ -0,0 +1,17 @@
+# Java Random Number Generator Test Questionnaire
+
+1. Generate randomized dataset of numbers (1 to 20)
+2. Should contain following distribution:
+- 1-12 = 83000
+- 13 = 1000
+- 14 = 500
+- 15 = 250
+- 16 = 100
+- 17 = 50
+- 18 = 25
+- 19 = 10
+- 20 = 5
+
+3. No two consecutive numbers should be the same in generated dataset
+4. Write to txt file with one value per line
+5. On console display all lines on which value 20 appears
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,75 @@
-import java.util.Random;
+import java.util.*;
+import java.util.stream.*;
 import java.io.*;
 import java.nio.file.*;
 
+/**
+ * SOURCES USED
+ * https://stackoverflow.com/questions/17750575/filewriter-in-java-not-writing-to-txt-file
+ * https://www.deadcoderising.com/how-to-generate-a-stream-of-random-numbers-in-java/
+ * https://stackoverflow.com/questions/20058366/shuffle-a-list-of-integers-with-java-8-streams-api
+ */
+
 
 public class Main {
+    public static final int firstDistribution = 83000;
+    public static final int secondDistribution = 1000;
 
     public static void main(String[] args) {
-//        try (BufferedWriter out = new BufferedWriter(
-//                new FileWriter("/Users/newowner/Documents/random_java/output.txt"))) {
-//            out.write("Hello World");
-//        } catch (IOException e) {
-//            e.printStackTrace();
+//        IntStream mine = getRandomStream();
+//        int [] mi = mine.toArray();
+//        for (int i: mi) {
+//            System.out.println(i);
+//
 //        }
+//
+        String[] array = new String[3];
+        array[0] = "1";
+        array[1] = "2";
+        array[2] = "3";
+
+        List<Integer> nums = getDataset();
+        for (int num : nums) {
+            System.out.println(num);
+        }
 
         try {
 
             FileWriter writer = new FileWriter("/Users/newowner/Documents/RandomGen/src/output.txt", true);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
 
-            bufferedWriter.write("Hello World!");
-            bufferedWriter.newLine();
-            bufferedWriter.write("See You!");
+            // Loop over the elements in the string array and write each line.
+            for (String line : array) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
 
             bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<Integer> getDataset() {
+        Random gen = new Random();
+
+        IntStream firstDistribution = gen.ints(20, 1, 13);
+        IntStream secondDis = gen.ints(10, 13, 14);
+
+        IntStream total = IntStream.concat(firstDistribution, secondDis);
+
+        List<Integer> integers =
+                        total                                   // <-- take our combined IntStream dataset
+                        .boxed()                                // <-- converts them to Integers
+                        .collect(Collectors.toList());          // <-- collects the values to a list
+
+        Collections.shuffle(integers);
+
+        return integers;
+
+    }
+
+    public static IntStream getRandomStream() {
+        return new Random().ints(50,1,20 + 1);
     }
 }
